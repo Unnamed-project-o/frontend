978fede73d9fb83a12eebe59ba0ee613863f03c4
jackhu <jackhu.521.rose@gmail.com>
2026-03-10 22:43:51 +0800

    Backend: DDD first devops: Fundamental construct.
    ---
    NA
    
    ---
    Commiter: Jack

--- vue.config.js
+++ vue.config.js
@@ -3,22 +3,15 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  //把 devServer 配置直接加在这里，和 transpileDependencies 并列
   devServer: {
-    port: 8080, // 前端跑在 8080
+    port: 8080,
     proxy: {
       '/api': {
-        // 1. 这里一定要填你【虚拟机】的 IP，不能填 localhost
+        /** TODO3: 修改这个为配置参数， ./run.sh <ip地址> 就可以直接替换这里，比如: ./run.sh 192.168.121.188 */
         target: 'http://192.168.121.188:8081', 
         changeOrigin: true,
         pathRewrite: {
-          // 2. 这里的坑请注意：
-          // 如果你的后端接口就是 /api/login，那就用下面这行（不用改）：
           '^/api': '' 
-          
-          // 如果你的后端接口是 /login (没有api前缀)，请注释掉上面那行，改用下面这就行：
-          // '^/api': '' 
         }
       }
     }
