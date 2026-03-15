08f4f4f1f484f10f22aca55cdc68150ba76e1d10
kkk <doczhangyh@gmail.com>
2026-03-15 11:55:49 +0800

    feat: implement dynamic backend IP configuration(TODO3) - Updated 'run.sh' to accept IP input and set it as an environment variable - Modified 'vue.config.js' to read the IP dynamically instead of using a hardcoded target

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+const backendIp = process.env.BACKEND_IP || '127.0.0.1';
 
 
 module.exports = defineConfig({
@@ -8,7 +9,7 @@ module.exports = defineConfig({
     proxy: {
       '/api': {
         /** TODO3: 修改这个为配置参数， ./run.sh <ip地址> 就可以直接替换这里，比如: ./run.sh 192.168.121.188 */
-        target: 'http://192.168.121.188:8081', 
+        target: `http://${backendIp}:8081`, 
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
